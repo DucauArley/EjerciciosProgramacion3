475db01323fe7f3be836cbba9c5fda22f0fa9e91
DucauArley <arleyducau@gmail.com>
2019-03-31 20:19:42 -0300

    Ejercicios

---
+++ Ejercicios php/Clases/clase1/Clases/Alumno.php
@@ -0,0 +1,13 @@
+<?php
+class Alumno extends Persona
+{
+	public $legajo;
+
+	public function __construct($nombre, $apellido, $dni, $legajo)
+	{
+		Parent::__construct($nombre, $apellido, $dni);
+		$this->legajo = $legajo;
+	}
+
+}
+?>
---
+++ Ejercicios php/Clases/clase1/Clases/Humano.php
@@ -0,0 +1,13 @@
+<?php
+class Humano
+{
+	public $nombre;
+	public $apellido;
+
+	public function __construct($nombre, $apellido)
+	{
+		$this->nombre = $nombre;
+		$this->apellido = $apellido;
+	}
+}
+?>
