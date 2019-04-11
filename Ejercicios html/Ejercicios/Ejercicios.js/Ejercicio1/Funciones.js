c3ff29229d17dba8d91bf5f84915484a05f1e2cd
DucauArley <arleyducau@gmail.com>
2019-04-11 17:50:16 -0300

    Ejercicios

---
+++ Ejercicios html/Ejercicios/Ejercicios.js/Ejercicio1/Funciones.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",function(){
+	var btnAlert = document.getElementById("btnAlert");
+	btnGuardar.addEventListener("click",alerta);
+});
+
+	function alerta()
+	{
+		alert("Hola Mundo!!!!\nPuedo mostrar comillas 'simples'\nY comillas \"dobles\"");
+	}	
