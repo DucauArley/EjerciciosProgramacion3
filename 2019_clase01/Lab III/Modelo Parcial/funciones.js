8c2dcbb81c6f4f3e7016849ecf9a16e8827dd37c
DucauArley <arleyducau@gmail.com>
2019-04-26 21:54:59 -0300

    Modelo de Parcial

---
+++ 2019_clase01/Lab III/Modelo Parcial/funciones.js
@@ -0,0 +1,76 @@
+window.addEventListener("load", carga);
+
+        var xml = new XMLHttpRequest();
+
+        function carga()
+        {
+            var boton = document.getElementById("btn").addEventListener("click", validarUsuario);
+        }
+
+        function validarUsuario()
+        {
+            var email = document.getElementById("email"); 
+            var password = document.getElementById("pass");
+
+            if(email.value == "email" && password.value == "password")
+            {
+                enviarPost();   
+                usuarioValido();
+            }
+            else
+            {
+                alert("Datos equivocados");
+            }
+        }
+
+        function usuarioValido()
+        {
+            window.location.replace("./index.html");
+        }
+
+        function enviarPost()
+            {
+                var usuario = document.getElementById("email");
+                var contraseña = document.getElementById("pass");
+
+                if(usuario.value != "" && contraseña.value != "")
+                {
+                    var parametros = {email: usuario.value,  password: contraseña.value};
+                    xml.open("POST", "http://localHost:1337/login",true);
+                    xml.onreadystatechange = callback;
+                    xml.send(JSON.stringify(parametros));
+                }
+                else
+                {
+                    alert("Debe ingresar el usuario y la contraseña");
+                }
+
+            }
+
+
+        function callback()
+            {
+                if(xml.readyState === 4)
+                {
+                    if(xml.status === 200)
+                    {
+                        console.log("LLego respuesta del servidor ", xml.readyState, xml.status, xml.responseText);
+                        var respuesta = xml.responseText;
+                        if (respuesta == "true") 
+                        {
+                            alert("Se ha registrado correctamente");
+                        }
+                        else
+                        {
+                            alert("Usuario o contraseña incorrecta");
+                        }
+                    }
+                    else
+                    {
+                        alert("Error del servidor ", xml.status);
+                    }
+                }
+            }
+
+
+
