a5f27b505e41562fa1351299eb217d28636c2206
DucauArley <arleyducau@gmail.com>
2019-05-26 19:50:21 -0300

    Parcial

--- Ejercicios php/Clases/Parcial/cargarProducto.php
+++ Ejercicios php/Clases/Parcial/cargarProducto.php
@@ -4,9 +4,16 @@
 	include "producto.php";
 
 	$id = $_POST["id"];
-
+	$nombreImg = $id . "-" . date("d.m.y");
+	$imagenVieja = $_FILES["imagen"]["name"];
+	$imagenVieja = explode(".", $imagenVieja);
 	$imagen = $_FILES["imagen"];
-	$imagen["name"] = $id . "." . date("d/m/y");
+	$imagen["name"] = $nombreImg;
+	$destino = "./fotos/" . $nombreImg . "." . end($imagenVieja); 
+
+	move_uploaded_file($imagen["tmp_name"], $destino);
+
+	$imagen["tmp_name"] = $destino;
 
 	$imagen = json_encode($imagen);
 
@@ -14,13 +21,7 @@
 
 	$datos = $producto->id . " " . $producto->nombre . " " . $producto->precio . " " . $producto->imagen . " " . $producto->usuario . PHP_EOL;
 
-	foreach ($_FILES as $item)
-	{
-		//El problema esta aca, el item no tiene el nombre por el que se lo tiene que cambiar, poreso no lo reconoce cuando lo llamo en 
-		//el modificar producto
-		var_dump($item);
-		//move_uploaded_file($item["tmp_name"], "./fotos/" . $item["name"]);
-	}
+	//rename($_FILES["imagen"]["tmp_name"], "./fotos/" . $nombreImg);
 
 	var_dump($datos);
 
--- Ejercicios php/Clases/Parcial/modificarProducto.php
+++ Ejercicios php/Clases/Parcial/modificarProducto.php
@@ -5,27 +5,40 @@
 	$productos = leer("productos.txt");
 
 	$id = $_POST["id"];
+	$nombre = $_FILES["imagen"]["name"];
 	$imagen = $_FILES["imagen"];
-	$imagen["name"] = $id . date("d/m/y");
+	$origen = $_FILES["imagen"]["tmp_name"];
+	$nombreImg = $id . "-" . date("d.m.y");
+	$imagen["name"] = $nombreImg;
+	$nombre = explode(".", $nombre);
 	$esta = 0;
 	$contador = 0;
+	$destino = "./fotos/" . $nombreImg . "." . end($nombre);
+	$vec = array();
+
+	$imagen["tmp_name"] = $destino;
 
 	foreach ($productos as $item) 
 	{
 		if($item[0] == $id)
 		{
-			$imagenMod = json_decode($item[3], true);
-			move_uploaded_file($imagenMod["tmp_name"], "./backUpFotos/" . $imagenMod["name"]);//aca iria todo lo de las imagenes
-			//Se me ocurre un explode para sacar el nombre y otro para sacar el tmp las cosas estan separadas por comas
-			$item[1] = $_POST["nombre"]; 
+			$imagenMod = json_decode($item[3], true);//Revisar bien esto
+			rename($imagenMod["tmp_name"], "./backUpFotos/" . $imagenMod["name"] . "." . end($nombre));
+			var_dump($imagenMod);
+			$item[1] = $_POST["nombre"];
 			$item[2] = $_POST["precio"];
 			$item[3] = $imagen;
 			$item[4] = $_POST["usuario"];
 			$esta ++;
+			$vec[] = $item;
+		}
+		else
+		{
+			$vec[] = $item;
 		}
 	}
 
-	move_uploaded_file($imagen["tmp_name"], "./fotos/" . $imagen["name"]);
+	move_uploaded_file($origen, $destino);
 
 	if($esta == 0)
 	{
@@ -33,8 +46,9 @@
 	}
 	else
 	{
-		foreach ($productos as $item) 
+		foreach ($vec as $item) 
 		{
+			$item[3] = json_encode($item[3]);
 			$datos = $item[0] . " " . $item[1] . " " . $item[2] . " " . $item[3] . " " . $item[4] . PHP_EOL;
 
 			if($contador == 0)
