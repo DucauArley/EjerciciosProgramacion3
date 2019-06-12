6e9d37dee4283bcc76914992a9c12c395c8592ca
DucauArley <arleyducau@gmail.com>
2019-06-12 14:59:31 -0300

    Ejercicios

--- Ejercicios php/Clases/clase8/index.php
+++ Ejercicios php/Clases/clase8/index.php
@@ -2,14 +2,13 @@
 	namespace Firebase\JWT;
 	use \Firebase\JWT\JWT;
 	require "vendor/autoload.php";
-
+	/*usar el composer para el slim composer require slim/slim "^3.12"
+	para el firebase composer require firebase/php-jwt
+	*/
 	use Psr\Http\Message\ServerRequestInterface as Request;
 	use Psr\Http\Message\ResponseInterface as Response;
 
-	$config["displayErrorDetails"] = true;
-	$config["addContentLengthHeader"] = false;
-
-	$app = new \Slim\App(["settings" => $config]);
+	$app = new \Slim\App;
 
 	$app->post('/crearToken', function (Request $request, Response $response) 
 	{
@@ -33,27 +32,34 @@
     
      
 	});
-	$app->post('/verificarToken', function (Request $request, Response $response) {
-    $token =$request->getHeader('token');
-    var_dump($tokenDos[0]);
-    $token = $tokenDos[0];
+	$app->post('/verificarToken', function (Request $request, Response $response) 
+	{
+	    $tokenDos =$request->getHeader('token');
+	    var_dump($tokenDos[0]);
+	    $token = $tokenDos[0];
     
-    if(empty($token) || $token === ""){
-    	throw new Exception("error 0x1");  	
-    }
-    try{
-    	$tokenDeco = JWT::decode($token,
-    	"clave",
-    		['HS256']
-    	);
-    		
-	}catch(Exception $exception){
-		throw new Exception("error 0x2");
-	}
-    
-    return "ok";
-     
+	    if(empty($token) || $token === "")
+	    {
+	    	throw new Exception("error 0x1");  	
+	    }
+	    
+	    try
+	    {
+	    	$tokenDeco = JWT::decode($token,
+	    	"clave",
+	    		['HS256']
+	    	);
+	    		
+		}
+		catch(Exception $exception)
+		{
+			throw new Exception("error 0x2");
+		}
+	    
+	    return "ok";
+	     
 	});
+
 	$app->run();
 
 ?>
